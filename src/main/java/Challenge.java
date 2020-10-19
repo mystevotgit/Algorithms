a060e3ee8b7e42c942fa0057c779006dddfcef0a
mystevotgit <oyeolatunji@gmail.com>
2020-10-19 17:32:51 +0100

    sortCsvColumns

--- src/main/java/Challenge.java
+++ src/main/java/Challenge.java
@@ -1,70 +1,61 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public class Challenge {
-    public static String numerals(int n) {
-        System.out.println(n);
-        List<String> numerals = new ArrayList<>();
-        while (n >= 1000) {
-            numerals.add("M");
-            n -= 1000;
+class Challenge {
+    public static String sortCsvColumns( String csv_data ) {
+        System.out.println(csv_data);
+        List<List<String>> table = new ArrayList<>();
+        table.add(new ArrayList<>());
+        List<String> list = Arrays.asList(csv_data.split(","));
+        for (int i = 0; i < list.size(); i++) {
+            List<String> each = Arrays.asList(list.get(i).split("\n"));
+            if (each.size() == 1) {
+                table.get(table.size()-1).add(each.get(0).trim());
+            }else if (each.size() == 2) {
+                table.get(table.size()-1).add(each.get(0).trim());
+                table.add(new ArrayList<>());
+                table.get(table.size()-1).add(each.get(1).trim());
+            }
         }
-        if (n >= 900) {
-            numerals.add("CM");
-            n -= 900;
+        List<String> column = table.get(0);
+        SortedMap<String, List<String>> data = new TreeMap<>(new Comparator<String>() {
+            public int compare(String a, String b)
+            {
+                return a.compareToIgnoreCase(b);
+            }
+        });
+
+        for (int i = 0; i < column.size(); i++) {
+            List<String> columnData = new ArrayList<>();
+            for (int j = 1; j < table.size(); j++) {
+                columnData.add(table.get(j).get(i));
+            }
+            data.put(column.get(i), columnData);
         }
-        if (n >= 500) {
-            numerals.add("D");
-            n -= 500;
+        String result = "";
+        List<String> keys = Arrays.asList(data.keySet().stream().collect(Collectors.joining(",")));
+        Iterator x = keys.iterator();
+        while(x.hasNext()){
+            result += x.next();
         }
-        if (n >= 400) {
-            numerals.add("CD");
-            n -= 400;
+        List<List<String>> val = new ArrayList<>();
+        val.addAll(data.values());
+        for (int i = 0; i < val.get(0).size(); i++) {
+            result += "\n";
+            for (int j = 0; j < val.size(); j++) {
+                result += val.get(j).get(i);
+                result += ",";
+            }
+            result = result.substring(0, result.length()-1);
         }
-        while (n > 99) {
-            numerals.add("C");
-            n -= 100;
-        }
-        if (n >= 90) {
-            numerals.add("XC");
-            n -= 90;
-        }
-        if (n >= 50) {
-            numerals.add("L");
-            n -= 50;
-        }
-        if (n >= 40) {
-            numerals.add("XL");
-            n -= 40;
-        }
-        while (n >= 10) {
-            numerals.add("X");
-            n -= 10;
-        }
-        if (n == 9) {
-            numerals.add("IX");
-            n -= 9;
-        }
-        if (n >= 5) {
-            numerals.add("V");
-            n -= 5;
-        }
-        if (n == 4) {
-            numerals.add("IV");
-            n -= 4;
-        }
-        while (n > 0) {
-            numerals.add("I");
-            n--;
-        }
-        return "".join("", numerals);
+        return result;
     }
     public static void main(String[] args) {
-        System.out.println(numerals(2342));
-        System.out.println(numerals(1478));
-        System.out.println(numerals(8));
-        System.out.println(numerals(9));
-        System.out.println(numerals(99));
-        System.out.println(numerals(1988));
+        System.out.println(sortCsvColumns("Beth,Adam\n" +
+                "2,1"));
+        System.out.println(sortCsvColumns("Beth,Charles,Danielle,Adam,Eric\n" +
+                "        17945,10091,10088,3907,10132\n" +
+                "        2,12,13,48,11"));
+
     }
 }
