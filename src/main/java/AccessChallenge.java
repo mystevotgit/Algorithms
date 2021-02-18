51cfd3fe077ed22ae172ce960024b273c8dd2115
mystevotgit <oyeolatunji@gmail.com>
2021-02-18 01:07:18 +0100

    create phonebookchallenge

--- src/main/java/AccessChallenge.java
+++ src/main/java/AccessChallenge.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class AccessChallenge {
 
---
+++ src/main/java/PhonebookChallenge.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+class PhonebookChallenge {
+
+    private static Map<String, String> phonebook = new HashMap<>();
+
+    public static void main(String[] args){
+        // Add a contact to the phonebook.
+        phonebook.put("Olatunji", "+2348144533711");
+        phonebook.put("Ola", "08123456789");
+
+        // Remove a contact from the phonebook.
+        phonebook.remove("Olatunji");
+
+        // View all the entries in the phonebook.
+        Set<String> keys = phonebook.keySet();
+        keys.forEach(key -> System.out.println("Name: " + key + " --->> Phone Number: " + phonebook.get(key)));
+    }
+}
