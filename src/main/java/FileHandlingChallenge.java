e26b4c9df08c483a620ee62018106c22c6e3b2f6
mystevotgit <oyeolatunji@gmail.com>
2021-02-20 23:14:23 +0100

    attempted some java questiuons

--- src/main/java/FileHandlingChallenge.java
+++ src/main/java/FileHandlingChallenge.java
@@ -4,34 +4,42 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileHandlingChallenge {
-    private static final String FILE_NAME = "src/test/resources/fileToCreate.txt";
+    private static final String FILE_NAME = "src/test/resources/fileToTarget.txt";
+    private static final String TARGET_NAME = "src/main/resources/fileToTarget.txt";
 
     public static void main(String[] args) throws IOException {
-//        cleanUpFiles();
+        cleanUpFiles();
+        Path sourcePath = Paths.get(FILE_NAME);
+        Path targetPath = Paths.get(TARGET_NAME);
+//        Files.copy(targetPath, sourcePath);
+        File file = new File(FILE_NAME);
+        file.delete();
 
-//        File newFile = new File(FILE_NAME);
-//        FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME);
-//        System.out.println(newFile.createNewFile());
-
-        InputStream inputStream = new FileInputStream(FILE_NAME);
-        StringBuilder stringBuilder = new StringBuilder();
-        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
-            String line;
-            while((line = bufferedReader.readLine()) != null) {
-                stringBuilder.append(line).append("\n");
-            }
-
-            System.out.println(stringBuilder);
-        }
-        finally {
-            if (inputStream != null) {
-                try {
-                    inputStream.close();
-                }catch(IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+//        String str = "This is a super story. \n";
+//        FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME, true);
+//        byte[] data = str.getBytes();
+//        fileOutputStream.write(data);
+//
+//        InputStream inputStream = new FileInputStream(FILE_NAME);
+//        StringBuilder stringBuilder = new StringBuilder();
+//        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+//            String line;
+//            while((line = bufferedReader.readLine()) != null) {
+//                stringBuilder.append(line).append("\n");
+//            }
+//
+//            System.out.println(stringBuilder);
+//        }
+//        finally {
+//            if (inputStream != null) {
+//                try {
+//                    fileOutputStream.close();
+//                    inputStream.close();
+//                }catch(IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
 
 //        cleanUpFiles();
     }
