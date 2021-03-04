54e268b7ef8d6b3b96483912c515bdc82601d1b7
mystevotgit <oyeolatunji@gmail.com>
2021-03-04 08:06:05 +0100

    bubblesort algorithm

---
+++ src/main/java/BubbleSort.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] list = {5, 3, 6, 2, 9, 6, 4, 7, 1};
+        System.out.println(Arrays.asList(bubbleSort(list)).toString());
+        int[] result = bubbleSort(list);
+        for (int i = 0; i < result.length; i++){
+            System.out.println(result[i]);
+        }
+    }
+
+    public static int[] bubbleSort(int[] list) {
+        int n = list.length;
+        int temp = 0;
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 1; j < (n - i); j++) {
+//                if (list[j - 1] > list[j]) {
+//                    //swap elements
+//                    temp = list[j - 1];
+//                    list[j - 1] = list[j];
+//                    list[j] = temp;
+//                }
+//
+//            }
+//        }
+        return list;
+    }
+}
