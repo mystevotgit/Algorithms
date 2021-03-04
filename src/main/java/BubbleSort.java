eb3cb30a60b9d2c7c225091d362efe3d9b695658
mystevotgit <oyeolatunji@gmail.com>
2021-03-04 08:07:06 +0100

    modified bubblesort

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -13,17 +13,17 @@ public class BubbleSort {
     public static int[] bubbleSort(int[] list) {
         int n = list.length;
         int temp = 0;
-//        for (int i = 0; i < n; i++) {
-//            for (int j = 1; j < (n - i); j++) {
-//                if (list[j - 1] > list[j]) {
-//                    //swap elements
-//                    temp = list[j - 1];
-//                    list[j - 1] = list[j];
-//                    list[j] = temp;
-//                }
-//
-//            }
-//        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (list[j - 1] > list[j]) {
+                    //swap elements
+                    temp = list[j - 1];
+                    list[j - 1] = list[j];
+                    list[j] = temp;
+                }
+
+            }
+        }
         return list;
     }
 }
