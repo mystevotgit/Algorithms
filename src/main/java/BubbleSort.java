f3bca0df5b82f668973cc2ca3b5a5c13be313f80
mystevotgit <oyeolatunji@gmail.com>
2021-03-10 10:41:22 +0100

    modified bubblesort

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
         int[] list = {5, 3, 6, 2, 9, 6, 4, 7, 1};
-        System.out.println(Arrays.asList(bubbleSort(list)).toString());
         int[] result = bubbleSort(list);
         for (int i = 0; i < result.length; i++){
             System.out.println(result[i]);
