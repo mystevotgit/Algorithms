14e9531e28f01b03f62d849db378dcc817c03ce1
mystevotgit <oyeolatunji@gmail.com>
2021-03-20 23:55:25 +0100

    minstack and grouptitles modified

--- src/main/java/netflixchallenge/grouptitles/Solution.java
+++ src/main/java/netflixchallenge/grouptitles/Solution.java
@@ -9,46 +9,28 @@ class Solution {
             return new ArrayList<List<String>>();
 
         Map<String, List<String>> res = new HashMap<String, List<String>>();
-
-        int[] count = new int[26];
         for (String s : strs) {
-            Arrays.fill(count, 0);
-            for (char c : s.toCharArray()){
-                int index = c - 'a';
-                count[index]++;
-            }
-
-            StringBuilder delimStr = new StringBuilder("");
-            for (int i = 0; i < 26; i++) {
-                delimStr.append('#');
-                delimStr.append(count[i]);
-            }
-
-            String key = delimStr.toString();
-            if (!res.containsKey(key))
-                res.put(key, new ArrayList<String>());
-
-            res.get(key).add(s);
-        }
-        List<List<String>> result = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            result.add(new ArrayList<>());
-        }
-        for (int j = 0; j < 3; j++) {
-            List<List<String>> list =  res.values().stream().collect(Collectors.toList());
-            if (list.get(j).size() == 3) {
-                result.set(0, list.get(j));
-            }
-            if (list.get(j).size() == 2) {
-                result.set(2, list.get(j));
-            }
-            if (list.get(j).size() == 1) {
-                result.set(1, list.get(j));
+            System.out.println(s);
+            char[] tempArr = s.toCharArray();
+            Arrays.sort(tempArr);
+            String str = new String(tempArr);
+            System.out.println(str);
+            if (res.get(str) == null) {
+                res.put(str, new ArrayList<>());
+                res.get(str).add(s);
+            }else{
+                res.get(str).add(s);
             }
         }
-        System.out.println(result);
+        Set<String> keys = res.keySet();
+        keys.stream().sorted();
+        System.out.println(keys);
 
-        return result;
+        List<List<String>> list =  res.values().stream().collect(Collectors.toList());
+
+        System.out.println(list);
+
+        return list;
     }
 
     public static void main(String[] args) {
--- src/main/java/netflixchallenge/minstack/MinStack.java
+++ src/main/java/netflixchallenge/minstack/MinStack.java
@@ -1,37 +1,92 @@
 package netflixchallenge.minstack;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class MinStack {
     private int maxSize;
-    private PriorityQueue<Integer> stack;
+    private List<Integer> stack;
+    private PriorityQueue<Integer> priorityQueue;
 
     //constructor
     public MinStack(int maxSize) {
         // Write -- Your -- Code
         this.maxSize = maxSize;
-        this.stack = new PriorityQueue<>(10, new Comparator<Integer>(){
+        this.stack = new ArrayList<>();
+        this.priorityQueue = new PriorityQueue<>(maxSize, new Comparator<Integer>(){
             @Override
             public int compare(Integer o1, Integer o2) {
-                return o1 < o2 ? 1 : -1;
+                return o1 - o2;
             }
         });
     }
     //removes and returns value from stack
     public Integer pop(){
         // Write -- Your -- Code
-        return stack.poll();
+        int removed = stack.remove(stack.size()-1);
+        Iterator it = priorityQueue.iterator();
+        if (it.hasNext()) {
+            priorityQueue.remove(removed);
+        }
+        return removed;
     }
     //pushes value into the stack
     public void push(Integer value){
         // Write -- Your -- Code
-        stack.add(value);
+        if (priorityQueue.size() < maxSize) {
+            stack.add(value);
+            priorityQueue.add(value);
+        }
         return;
     }
     //returns minimum value in O(1)
     public int min(){
         // Write -- Your -- Code
-        return stack.peek();
+        System.out.println(priorityQueue.peek() + " is the min value\n");
+        if (!priorityQueue.isEmpty()) {
+            return priorityQueue.peek();
+        }else {
+            return stack.get(stack.size()-1);
+        }
     }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack(4);
+        minStack.push(2);
+        minStack.push(1);
+        minStack.push(1);
+        minStack.push(10);
+        minStack.push(5);
+
+
+        Iterator iterator = minStack.priorityQueue.iterator();
+        while(iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        System.out.println(minStack.stack.toString());
+
+        System.out.println(minStack.pop() + " is the poped value\n");
+        System.out.println(minStack.pop() + " is the poped value\n");
+
+        Iterator it = minStack.priorityQueue.iterator();
+        while(it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+        System.out.println(minStack.stack.toString());
+
+        minStack.min();
+    }
+
+//    public static boolean validateStackSequences(int[] pushed, int[] popped) {
+//        // write your code here
+//        Set<Integer> hashSet = new HashSet<>();
+////        hashSet.addAll(Arrays.asList(popped));
+//        if (pushed.length == popped.length && hashSet.size() == pushed.length){
+//            return true;
+//        }
+//
+//        return false;
+//    }
 }
+
