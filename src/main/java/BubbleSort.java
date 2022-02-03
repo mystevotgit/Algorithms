38920356e7b1d69bb29cac8821c043223beabecb
mystevotgit <oyeolatunji@gmail.com>
2022-02-03 08:46:03 +0100

    formating and algorithm update

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -1,10 +1,10 @@
-import java.util.Arrays;
+
 
 public class BubbleSort {
     public static void main(String[] args) {
         int[] list = {5, 3, 6, 2, 9, 6, 4, 7, 1};
         int[] result = bubbleSort(list);
-        for (int i = 0; i < result.length; i++){
+        for (int i = 0; i < result.length; i++) {
             System.out.println(result[i]);
         }
     }
