62d3f49a0020b36753efc1df81d2543eb40b8b94
mystevotgit <oyeolatunji@gmail.com>
2022-03-14 03:21:28 +0100

    Reformat

--- src/main/java/Challenge.java
+++ src/main/java/Challenge.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 class Challenge {
-    public static String sortCsvColumns( String csv_data ) {
+    public static String sortCsvColumns(String csv_data) {
         System.out.println(csv_data);
         List<List<String>> table = new ArrayList<>();
         table.add(new ArrayList<>());
@@ -10,17 +10,16 @@ class Challenge {
         for (int i = 0; i < list.size(); i++) {
             List<String> each = Arrays.asList(list.get(i).split("\n"));
             if (each.size() == 1) {
-                table.get(table.size()-1).add(each.get(0).trim());
-            }else if (each.size() == 2) {
-                table.get(table.size()-1).add(each.get(0).trim());
+                table.get(table.size() - 1).add(each.get(0).trim());
+            } else if (each.size() == 2) {
+                table.get(table.size() - 1).add(each.get(0).trim());
                 table.add(new ArrayList<>());
-                table.get(table.size()-1).add(each.get(1).trim());
+                table.get(table.size() - 1).add(each.get(1).trim());
             }
         }
         List<String> column = table.get(0);
         SortedMap<String, List<String>> data = new TreeMap<>(new Comparator<String>() {
-            public int compare(String a, String b)
-            {
+            public int compare(String a, String b) {
                 return a.compareToIgnoreCase(b);
             }
         });
@@ -35,7 +34,7 @@ class Challenge {
         String result = "";
         List<String> keys = Arrays.asList(data.keySet().stream().collect(Collectors.joining(",")));
         Iterator x = keys.iterator();
-        while(x.hasNext()){
+        while (x.hasNext()) {
             result += x.next();
         }
         List<List<String>> val = new ArrayList<>();
@@ -46,10 +45,11 @@ class Challenge {
                 result += val.get(j).get(i);
                 result += ",";
             }
-            result = result.substring(0, result.length()-1);
+            result = result.substring(0, result.length() - 1);
         }
         return result;
     }
+
     public static void main(String[] args) {
         System.out.println(sortCsvColumns("Beth,Adam\n" +
                 "2,1"));
@@ -57,5 +57,65 @@ class Challenge {
                 "        17945,10091,10088,3907,10132\n" +
                 "        2,12,13,48,11"));
 
+        RoundPeg roundPeg = new RoundPeg(5);
+        RoundHole roundHole = new RoundHole(5);
+        System.out.println(roundHole.isFit(roundPeg));
+        SquarePeg squarePeg = new SquarePeg(5);
+        SquareAdapter squareAdapter = new SquareAdapter(squarePeg);
+        System.out.println(roundHole.isFit(squareAdapter));
+    }
+
+    static class RoundPeg {
+        private double radius;
+
+        public RoundPeg(int radius) {
+            this.radius = radius;
+        }
+
+        public Double getRadius() {
+            return this.radius;
+        }
+    }
+
+    static class RoundHole {
+        private RoundPeg peg;
+        private int radius;
+
+        public RoundHole(int radius) {
+            this.radius = radius;
+        }
+
+        public Boolean isFit(RoundPeg roundPeg) {
+            if (roundPeg.getRadius() <= radius) {
+                this.peg = roundPeg;
+                return true;
+            }
+            return false;
+        }
+    }
+
+    static class SquarePeg {
+        private int width;
+
+        public SquarePeg(int width) {
+            this.width = width;
+        }
+
+        public int getWidth() {
+            return this.width;
+        }
+    }
+
+    static class SquareAdapter extends RoundPeg {
+        private SquarePeg squarePeg;
+
+        public SquareAdapter(SquarePeg squarePeg) {
+            super(0);
+            this.squarePeg = squarePeg;
+        }
+
+        public Double getRadius() {
+            return squarePeg.width * (Math.sqrt(2) / 2);
+        }
     }
 }
